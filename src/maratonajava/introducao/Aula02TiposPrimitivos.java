67da5775268668422b66456e532317d9c972982a
caio medeiros <caiokakalima10@gmail.com>
2026-01-23 17:29:25 -0300

    Video 11 completed.

--- src/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -3,7 +3,7 @@ package maratonajava.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
-        int idade = 10;
+        int idade = 15;
         System.out.println(idade);
     }
 }
