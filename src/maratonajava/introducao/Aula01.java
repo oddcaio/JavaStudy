b86d9b01880bb8fc42bee717afc161b95ba6f0aa
caio medeiros <caiokakalima10@gmail.com>
2026-01-23 18:18:43 -0300

    Video 12 Completed

--- src/maratonajava/introducao/Aula01.java
+++ src/maratonajava/introducao/Aula01.java
@@ -3,5 +3,6 @@ package maratonajava.introducao;
 public class Aula01 {
     public static void main(String[] args) {
         System.out.println("23/1/2026");
+
     }
 }
--- src/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -3,18 +3,20 @@ package maratonajava.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
-        int idade = 10;
-        long numeroGrande = 100000;
-        double salarioDouble = 2000;
-        float salarioFloat = 2500;
-        byte idadeByte = 10;
-        short idadeShort = 10;
+        int idade = (int) 10000000000L;
+        long numeroGrande = (long)155.23;
+        double salarioDouble = 2000.0D;
+        float salarioFloat = (float) 2500.0F;
+        byte idadeByte = 127;
+        short idadeShort = 32000;
         boolean verdadeiro = true;
         boolean falso = false;
-        char caractere = 'M';
+        char caractere = '\u0041';
 
         System.out.println(idade);
         System.out.println(verdadeiro);
         System.out.println("char "+caractere);
+        System.out.println(idade);
+        System.out.println(salarioFloat);
     }
 }
