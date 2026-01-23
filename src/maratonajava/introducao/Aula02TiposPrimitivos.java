e7b11228fd2eb84654b26a8942f27a39cd23327d
caio medeiros <caiokakalima10@gmail.com>
2026-01-23 18:05:20 -0300

    Video 11 completed

--- src/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -3,7 +3,18 @@ package maratonajava.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
-        int idade = 15;
+        int idade = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'M';
+
         System.out.println(idade);
+        System.out.println(verdadeiro);
+        System.out.println("char "+caractere);
     }
 }
