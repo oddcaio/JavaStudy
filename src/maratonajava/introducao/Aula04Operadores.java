d71838defb4712f9c07e6edf4d218d65aaf0407c
Caio Medeiros <caiokakalima10@gmail.com>
2026-01-25 01:55:00 -0300

    Video 15 completed

---
+++ src/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,15 @@
+package maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        // + - / *
+        int numero01 = 10;
+        int numero02 = 20;
+        int resultado = numero01 + numero02;
+        System.out.println(resultado);
+
+        // o resultado de operção entre dois valores inteiros o resultado sempre vai ser em numero inteiro.
+        // ou seja, não apareceria "0,5" se dividisse "int 10 por int 20", precisa ser passado como double por cast ou declarando.
+        
+    }
+}
