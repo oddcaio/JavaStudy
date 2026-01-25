ac768cba1316cda9ef8fa38fcb04929553f19a7c
Caio Medeiros <caiokakalima10@gmail.com>
2026-01-25 04:14:47 -0300

    Video 19 Completed

--- src/maratonajava/introducao/Aula04Operadores.java
+++ src/maratonajava/introducao/Aula04Operadores.java
@@ -41,6 +41,25 @@ public class Aula04Operadores {
 
         boolean isPlaystationCincoCompravel = valoresTotalContaCorrente > valorPlaystation || valoresTotalContaPoupanca > valorPlaystation;
         System.out.println("isPlaystationCincoCompravel " +isPlaystationCincoCompravel);
-        
+
+        // =, =+, -=, *=, /=, %=
+        double bonus = 1800; //1800
+        bonus += 1000; //2800
+        bonus -= 1000; //1800
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+
+        System.out.println(bonus);
+
+        //
+        int contador = 8;
+        contador+= 1; //contador = contador + 1
+        contador++;
+        contador--;
+        ++contador;
+        --contador;
+        int contador2 = 0;
+        System.out.println(++contador2);
     }
 }
