d479c47d078fe3bb4a267a1ced392c3f08696ae9
Caio Medeiros <caiokakalima10@gmail.com>
2026-01-26 01:14:33 -0300

    Lesson 05/01  Completed

--- src/maratonajava/introducao/Aula05EstruturasCondicionais01.java
+++ src/maratonajava/introducao/Aula05EstruturasCondicionais01.java
@@ -1,16 +1,18 @@
 package maratonajava.introducao;
 
-public class Aula05EstruturasCondicionais {
+public class Aula05EstruturasCondicionais01 {
     public static void main(String[] args) {
         
         int idade = 12;
         boolean isAutorizadoComprarBebida = idade >= 18; 
-        if (isAutorizadoComprarBebida) {
+        if (isAutorizadoComprarBebida != false) {
             System.out.println("Autorizado a comprar bebida alcólica");
+        } else{
+            System.out.println("Não autorizado a comprar bebida alcólica");
         }
 
         // ! 
-        if(isAutorizadoComprarBebida == false){
+        if(!isAutorizadoComprarBebida){
             System.out.println("Não autorizado a comprar bebida alcólica");
         }
         
