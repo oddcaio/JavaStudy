a7a77bc8ae0efeb6d47a97069cfcd308cdbddbf6
caio medeiros <caiokakalima10@gmail.com>
2026-01-26 18:32:43 -0300

    Video 27 Completed

---
+++ src/maratonajava/introducao/Aula06EstruturasDeRepeticao01.java
@@ -0,0 +1,12 @@
+package maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao01 {
+    public static void main(String[] args) {
+        // while, do while, for
+        int count = 0;
+        while(count < 10){
+            System.out.println(++count);
+        }
+    }
+    
+}
