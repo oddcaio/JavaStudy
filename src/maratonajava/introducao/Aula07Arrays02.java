d425738a984ddcadaab457164957917124819697
Caio Medeiros <caiokakalima10@gmail.com>
2026-01-27 05:33:04 -0300

    Video 33 completed

---
+++ src/maratonajava/introducao/Aula07Arrays02.java
@@ -0,0 +1,16 @@
+package maratonajava.introducao;
+
+public class Aula07Arrays02 {
+public static void main(String[] args) {
+
+    // byte, short, int, long, float, e double 0
+    // char '\u0000' ''
+    // boolean false
+    // String null
+
+    String[] nomes = new int[3];
+    System.out.println(nomes[0]);
+    System.out.println(nomes[1]);
+    System.out.println(nomes[2]);
+}
+}
