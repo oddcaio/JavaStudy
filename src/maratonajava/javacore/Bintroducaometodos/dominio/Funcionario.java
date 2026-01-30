259482c0b1a5e18e06eeb5be789cc230381f4065
Caio Medeiros <caiokakalima10@gmail.com>
2026-01-29 23:23:16 -0300

    53 - Object-Oriented Programming - Methods Part 10 - Exercise

---
+++ src/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
@@ -0,0 +1,32 @@
+package maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Funcionario {
+    public String nome;
+    public int idade;
+    public double[] salarios;
+
+    public void imprime() {
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+        if (salarios != null) {
+            return;
+        }
+        for (double salario : salarios) {
+            System.out.print(salario + " ");
+        }
+        imprimeMediaSalario();
+    }
+
+    public void imprimeMediaSalario() {
+        double media = 0;
+        if (salarios != null) {
+            return;
+        }
+        for (double salario : salarios) {
+            media += salario;
+        }
+        media /= salarios.length;
+        System.out.println("\nMédia salarial " + media);
+
+    }
+}
---
+++ src/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTeste04.java
@@ -0,0 +1,8 @@
+package maratonajava.javacore.Bintroducaometodos.test;
+
+public class CalculadoraTeste04 {
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 2;
+    }
+}
