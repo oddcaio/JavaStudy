e7e439f5ef4dc9f3b20cbc69c5db4b89b51ff2dc
caio medeiros <caiokakalima10@gmail.com>
2026-02-02 12:09:50 -0300

    55 - Object-Oriented Programming - Private, Get, and Set Access Modifiers Part 2

--- src/maratonajava/javacore/Bintroducaometodos/dominio/Pessoa.java
+++ src/maratonajava/javacore/Bintroducaometodos/dominio/Pessoa.java
@@ -13,7 +13,16 @@ public class Pessoa {
     }
 
     public void setIdade(int idade){
+        if(idade < 0){
+            System.out.println("Idade inválida");
+            return;
+        }
         this.idade = idade;
     }
     
+    public String getNome(){
+        return this.nome;
+    }
+
+    public int getIdade(){return this.idade; }
 }
