eabd7769d545b266d2d7660d217e893a3da31ac9
caio medeiros <caiokakalima10@gmail.com>
2026-02-03 17:03:39 -0300

    Video 59 - Object Orientation - Constructors pt 02 - Overloading

--- src/maratonajava/javacore/Dconstrutores/dominio/Anime.java
+++ src/maratonajava/javacore/Dconstrutores/dominio/Anime.java
@@ -1,29 +1,36 @@
 package maratonajava.javacore.Dconstrutores.dominio;
 
 public class Anime {
+
     private String nome;
     private String tipo;
     private int episodios;
     private String genero;
-    
-    public Anime(String nome, String tipo, int episodios, String generos){
-        System.out.println("Dentro do construtor");
+    private String estudio;
+
+    public Anime(String nome, String tipo, int episodios, String genero) {
+        this();
         this.nome = nome;
         this.tipo = tipo;
         this.episodios = episodios;
         this.genero = genero;
     }
 
-    public Anime (){
-        
+    public Anime(String nome, String tipo, int episodios, String genero, String estudio) {
+        this(nome, tipo, episodios, genero);
+        this.estudio = estudio;
     }
 
-        
-        public void imprime() {
-            System.out.println(this.nome);
-            System.out.println(this.tipo);
-            System.out.println(this.episodios);
-            System.out.println(this.genero);
+    public Anime() {
+        System.out.println("Dentro do construtor sem argumentos");
+    }
+
+    public void imprime() {
+        System.out.println(this.nome);
+        System.out.println(this.tipo);
+        System.out.println(this.episodios);
+        System.out.println(this.genero);
+        System.out.println(this.estudio);
     }
 
     public void setNome(String nome) {
--- src/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
+++ src/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -4,10 +4,8 @@ import maratonajava.javacore.Dconstrutores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Haikyuu", "TV", 12, "Ação");
-        Anime anime2 = new Anime();
+        Anime anime = new Anime("Haikyuu", "TV", 12, "Ação", "Production IG");
         // anime.init("Akudama Drive", "TV", 12, "Ação");
         anime.imprime();
-        anime2.imprime();
     }
 }
